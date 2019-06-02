0acb6b484c2a1f5332761495072d65faa82a3ba5
natcancein <nataliaconstanz@gmail.com>
2019-06-01 20:01:16 -0400

    change json to deploy

---
+++ build/precache-manifest.8929d040772b2417877cf20535e84fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c936093622fbc529ac1cce92a87f103",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "1e2d1352e67faa374aa2",
+    "url": "/javascript-calculator/static/css/main.f0b8b0c6.chunk.css"
+  },
+  {
+    "revision": "ee91dd83532a60c97b7e",
+    "url": "/javascript-calculator/static/js/2.f9c1b42c.chunk.js"
+  },
+  {
+    "revision": "1e2d1352e67faa374aa2",
+    "url": "/javascript-calculator/static/js/main.c93f95c7.chunk.js"
+  },
+  {
+    "revision": "af3dc86e0aee640c7c09",
+    "url": "/javascript-calculator/static/js/runtime~main.b6bacda3.js"
+  }
+]);
